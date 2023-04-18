e8eb8ca9bd1ddf669a86a5e0819ee68470caf7db
ThinhLe30 <thinhmnsd2002@gmail.com>
2023-04-18 21:53:09 +0700

    init project

---
+++ src/main/java/com/brooklyn/SpringBootRestApiBlogApplication.java
@@ -0,0 +1,13 @@
+package com.brooklyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brooklyn/SpringBootRestApiBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brooklyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
